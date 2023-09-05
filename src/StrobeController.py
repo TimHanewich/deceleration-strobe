255f4e551969f7b983005535171fc352e72970b0
Tim Hanewich <tahanewich@live.com>
2023-09-05 17:04:15 -0400

    Adjusted off period too

--- src/StrobeController.py
+++ src/StrobeController.py
@@ -30,10 +30,10 @@ class StrobeController:
                     if self.mode != MODE_STATIONARY:
                         break
 
-                # turn off for 3 seconds
+                # turn off
                 GPIO.output(self.gpio, GPIO.LOW)
                 tstart:float = time.time()
-                while ((time.time() - tstart) < 2.0):
+                while ((time.time() - tstart) < 3.0):
                     time.sleep(0.01)
                     if self.mode != MODE_STATIONARY:
                         break
