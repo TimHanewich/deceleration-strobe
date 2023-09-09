961a2c1f900eb66464390ac51695d4d3c83c9afb
Tim Hanewich <tahanewich@live.com>
2023-09-09 15:21:18 -0400

    Swapped priority

--- src/StrobeCalculator.py
+++ src/StrobeCalculator.py
@@ -9,12 +9,11 @@ class StrobeCalculator:
         self._last_ticks_ms:int = None
 
     def feed(self, speed_mph:float, ticks_ms:int) -> None:
-
         if self._last_speed_mph != None and self._last_ticks_ms != None and ticks_ms > self._last_ticks_ms:
             acceleration_mph_per_second:float = (speed_mph - self._last_speed_mph) / ((ticks_ms - self._last_ticks_ms)/1000)
-            if speed_mph < settings.stationary_threshold: # we are stationary, or below 10 MPH, so put on the stationary mode.
+            if acceleration_mph_per_second < (settings.deceleration_threshold * -1): # shaving off X MPH per second
                 self.mode = StrobeController.MODE_STATIONARY
-            elif acceleration_mph_per_second < (settings.deceleration_threshold * -1): # shaving off X MPH per second
+            elif speed_mph < settings.stationary_threshold: # we are stationary, or below 10 MPH, so put on the stationary mode.
                 self.mode = StrobeController.MODE_ON
             else: # not decelerating hard enough. So turn off!
                 self.mode = StrobeController.MODE_OFF
