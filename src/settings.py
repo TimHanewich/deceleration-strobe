0a9408562b8f77f7885c17bc90f6d238e58f08cb
Tim Hanewich <tahanewich@live.com>
2023-09-13 15:32:09 -0400

    Went to GPIO 22 for led

--- src/settings.py
+++ src/settings.py
@@ -2,4 +2,4 @@ deceleration_threshold:float = 1.0 # The deceleration, in MPH per second, that y
 stationary_threshold:float = 10.0 # in MPH. Any speed below this will be considered "stationary" and will play the stationary pattern.
 
 gpio_gps_uart:int = 17 # general purpose pin (GP #, not pin #) that is the receiving data from the NEO-6M GPS module via UART
-gpio_led:int = 25 # general purpose pin (GP #, not pin #) that controls the strobe LED (wired to base of transistor)
+gpio_led:int = 22 # general purpose pin (GP #, not pin #) that controls the strobe LED (wired to base of transistor)
